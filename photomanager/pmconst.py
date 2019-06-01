e5c124b35bbdef80c68caed8c58e6afe4c3561fe
zhangchi <czhang@malong.com>
2019-06-01 18:32:14 +0800

    add skip list for skip thumb file by iphone

--- photomanager/pmconst.py
+++ photomanager/pmconst.py
@@ -2,6 +2,7 @@ PMDBNAME = "pmindex.db"
 PM_TODO_LIST = "filelist.txt"
 
 SUPPORT_EXTS = ["jpg", "jpeg", "gif", "png", "pcx", "tif", "bmp", "tiff", "webp", "ico"]
+SKIP_LIST = ['._thumb']
 
 TODO_INX_NAME = "todo_index"
 
